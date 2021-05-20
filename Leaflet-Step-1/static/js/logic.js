9f5e6a5f1fde47ef468326ec216f74587d227336
crogar <58400421+crogar@users.noreply.github.com>
2021-05-20 13:13:08 -0400

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,12 +29,41 @@ var myMap = L.map("mapid", {
 let geojson_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 L.control.layers(baseMaps).addTo(myMap);
 
-function setcolors(){
-
+function setcolors(depth){
+    var circleColor = "green";
+    if (depth > 90) {
+        circleColor = "red";
+    }
+    else if (depth > 70) {
+        circleColor = "#eb5e34";
+    }
+    else if (depth > 50) {
+        circleColor = "#eb9f34";
+    }
+    else if (depth > 30) {
+        circleColor = "#ebd334";
+    }   
+    else if (depth > 10) {
+        circleColor = "#e2eb34";
+    }
+    return circleColor;
 } 
 
 d3.json(geojson_url).then(data => {
     console.log(data);
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: feature.properties.mag*3,
+                fillColor: setcolors(feature.geometry.coordinates[2]),
+                color: "black",
+                weight: 1
+            });
+        },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(`${feature.properties.place}<br>Magnitude: ${feature.properties.mag}<br>${new Date(feature.properties.time)}`);
+            }
+    }).addTo(myMap)
 });
   // Pass our map layers into our layer control -  Add the layer control to the map
 
